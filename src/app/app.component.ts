8445b3df5ccaf3871eea11428265098d6f0f601e
shalinkumar <sayhi2shalin@gmail.com>
2017-05-30 21:24:00 -0700

    tabs and carousel implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 
 import { Component } from '@angular/core';
+import { ICarouselConfig, AnimationConfig } from 'angular4-carousel';
+
 
 @Component({
   selector: 'my-app',
@@ -8,5 +10,27 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'angularjs 2 app works great!';
+// tiles = [
+//     {text: 'One', cols: 4, rows: 9, color: 'lightblue'},
+//     {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+//     {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+//     {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+//   ];
+public imageSources: string[] = [
+     'http://www.violinshoptampa.com/Violin%20Shop%20Tampa-15.jpg',
+     'http://gomighty.com/wp-content/themes/gomighty/lib/goal_images/files/SMusicPianoAntiqueshutterstock_-1920.jpg',
+     'https://d1llvcsapfiksz.cloudfront.net/vendors/samplephonics/deep-sax/images/DeepSax_mobile.jpg',
+     'https://www.abamet.ru/images/press/haas/press-releases/2013/gaboi-rigoutat.jpg'
+  ];
+  
+  public config: ICarouselConfig = {
+    verifyBeforeLoad: true,
+    log: false,
+    animation: true,
+    animationType: AnimationConfig.SLIDE,
+    autoplay: true,
+    autoplayDelay: 2000,
+    stopAutoplayMinWidth: 768
+  };
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule,JsonpModule } from '@angular/http';
 
 // Material 2 
-import { MdCoreModule } from '@angular2-material/core'
-import { MdButtonModule } from '@angular2-material/button';
-import { MdCardModule } from '@angular2-material/card';
-import { MdRadioModule } from '@angular2-material/radio';
-import { MdCheckboxModule } from '@angular2-material/checkbox'
-import { MdTooltipModule } from '@angular2-material/tooltip';
-import { MdSliderModule } from '@angular2-material/slider';
+
 import  'hammerjs' 
+import { MaterialModule,MdInputModule,MdCardModule,MdButtonModule,MdTabsModule } from '@angular/material';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 // appcomponent declarations 
 import { AppComponent } from './app.component';
@@ -23,6 +20,7 @@ import {DogDetailsComponent} from './dogs/dog-details-component';
 import {PetService} from './pet.service';
 import { Pet }          from './pet';
 import {routing} from './app.routes'
+import { CarouselModule } from 'angular4-carousel';
 
 //Decorator 
 @NgModule({
@@ -31,8 +29,9 @@ import {routing} from './app.routes'
     FormsModule,
     HttpModule,
     JsonpModule,
-    routing,MdCoreModule, MdCardModule, MdButtonModule, MdRadioModule,
-MdCheckboxModule, MdTooltipModule, MdSliderModule
+    routing,
+    MaterialModule,
+MdInputModule,MdCardModule,BrowserAnimationsModule,CarouselModule,MdButtonModule
   ],
   declarations: [
     AppComponent,
